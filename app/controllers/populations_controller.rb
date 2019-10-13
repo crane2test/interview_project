f09d34a7e3787ab06c5e7b773d51735c5f69db01
Eric Crane <eric.crane@mac.com>
2019-10-13 09:16:35 -0400

    Challenge 3
    Prevent XSS attack through year parameter

--- app/controllers/populations_controller.rb
+++ app/controllers/populations_controller.rb
@@ -4,6 +4,10 @@ class PopulationsController < ApplicationController
 
   def show
     @year = params[:year].html_safe
+		
+		# Convert to integer to prevent nasties in URL parameters
+		@year = @year.to_i.to_s
+		
     @population = Population.get(@year,true)
   end
 end
