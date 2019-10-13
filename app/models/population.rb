0033cc6d6469cd1fd60e09cc37091e1f34373472
Eric Crane <eric.crane@mac.com>
2019-10-13 07:49:24 -0400

    Challenge 1
    Refactor Population model to make finding the closest year faster.

--- app/models/population.rb
+++ app/models/population.rb
@@ -3,18 +3,24 @@ class Population < ApplicationRecord
   def self.min_year
     Population.all.map(&:year).min.year
   end
+	
+	# Look for the closest year for which we have data.
+	# The closest year might be the year requested, 
+	# or it might be an earlier year.
+	def self.find_closest_year( year )
+		d = Date.new( year )
+		return Population.where( 'year <= ?', d ).order( 'year DESC' ).first
+	end
 
+	# Get the population for a particular year.
+	# NOTE that if there's no data for a given year, 
+	# we're going to extrapolate.
   def self.get(year)
     year = year.to_i
 
     return 0 if year < min_year
 
-    pop = nil
-    until pop
-      pop = Population.find_by_year(Date.new(year))
-      year = year - 1
-    end
-
+    pop = find_closest_year( year )
     return pop.population if pop
 
     nil
